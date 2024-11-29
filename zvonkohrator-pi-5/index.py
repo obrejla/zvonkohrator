e9bf002b1f262369504b0f9464400f22d9f0d6ec
Ondrej Brejla <ondrej.brejla@memsource.com>
2024-11-29 17:59:12 +0100

    Moved example out of index.

--- zvonkohrator-pi-5/index.py
+++ zvonkohrator-pi-5/index.py
@@ -13,13 +13,3 @@ lcd = LCD1602Mock(16, 2)
 midi_command_handler = MidiCommandHandler(midi_player, lcd)
 midi_listener = MidiListener(midi_command_handler)
 midi_listener.listen()
-
-# midi_out = rtmidi.MidiOut()
-# midi_out.open_port(ports_dict["MIDI In"]);
-#
-# note_on = [0x92, 48, 100]
-# note_off = [0x82, 48, 0]
-#
-# midi_out.send_message(note_on)
-# time.sleep(1)
-# midi_out.send_message(note_off)
