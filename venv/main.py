2d9dd83609edf934a9b3ed4563da443dfc8c0111
Ondrej Brejla <ondrej.brejla@memsource.com>
2024-11-29 10:18:02 +0100

    Improved naming.

--- venv/main.py
+++ venv/main.py
@@ -2,18 +2,18 @@ from machine import Pin, Timer
 import select
 import sys
 
-C_led = Pin(4, Pin.OUT)
-Cis_led = Pin(5, Pin.OUT)
-D_led = Pin(6, Pin.OUT)
-Dis_led = Pin(7, Pin.OUT)
-E_led = Pin(8, Pin.OUT)
-F_led = Pin(9, Pin.OUT)
-Fis_led = Pin(10, Pin.OUT)
-G_led = Pin(11, Pin.OUT)
-Gis_led = Pin(12, Pin.OUT)
-A_led = Pin(13, Pin.OUT)
-B_led = Pin(14, Pin.OUT)
-H_led = Pin(15, Pin.OUT)
+C2 = Pin(4, Pin.OUT)
+Cis2 = Pin(5, Pin.OUT)
+D2 = Pin(6, Pin.OUT)
+Dis2 = Pin(7, Pin.OUT)
+E2 = Pin(8, Pin.OUT)
+F2 = Pin(9, Pin.OUT)
+Fis2 = Pin(10, Pin.OUT)
+G2 = Pin(11, Pin.OUT)
+Gis2 = Pin(12, Pin.OUT)
+A2 = Pin(13, Pin.OUT)
+B2 = Pin(14, Pin.OUT)
+H2 = Pin(15, Pin.OUT)
 
 # Set up the poll object
 poll_obj = select.poll()
@@ -32,44 +32,44 @@ while True:
         # data2 = int(data)
         # print(data2)
         if data == "36":
-            C_led.toggle()
+            C2.toggle()
         elif data == "37":
-            Cis_led.toggle()
+            Cis2.toggle()
         elif data == "38":
-            D_led.toggle()
+            D2.toggle()
         elif data == "39":
-            Dis_led.toggle()
+            Dis2.toggle()
         elif data == "40":
-            E_led.toggle()
+            E2.toggle()
         elif data == "41":
-            F_led.toggle()
+            F2.toggle()
         elif data == "42":
-            Fis_led.toggle()
+            Fis2.toggle()
         elif data == "43":
-            G_led.toggle()
+            G2.toggle()
         elif data == "44":
-            Gis_led.toggle()
+            Gis2.toggle()
         elif data == "45":
-            A_led.toggle()
+            A2.toggle()
         elif data == "46":
-            B_led.toggle()
+            B2.toggle()
         elif data == "47":
-            H_led.toggle()
+            H2.toggle()
         continue
     else:
         # do something if no message received (like feed a watchdog timer)
         continue
 
 #
-# C_led.toggle()
-# Cis_led.toggle()
-# D_led.toggle()
-# Dis_led.toggle()
-# E_led.toggle()
-# F_led.toggle()
-# Fis_led.toggle()
-# G_led.toggle()
-# Gis_led.toggle()
-# A_led.toggle()
-# B_led.toggle()
-# H_led.toggle()
+# C2.toggle()
+# Cis2.toggle()
+# D2.toggle()
+# Dis2.toggle()
+# E2.toggle()
+# F2.toggle()
+# Fis2.toggle()
+# G2.toggle()
+# Gis2.toggle()
+# A2.toggle()
+# B2.toggle()
+# H2.toggle()
