773e5e8235e28de98ba9a594de0fdd062fc85cce
Ondrej Brejla <ondrej@brejla.cz>
2024-12-05 09:30:00 +0100

    Cleanup.

--- zvonkohrator-pi-5/test.py
+++ zvonkohrator-pi-5/test.py
@@ -107,35 +107,3 @@ try:
     pause()
 except KeyboardInterrupt:
     lcd.clear()
-
-
-# Exception in thread Thread-1:
-# Traceback (most recent call last):
-#   File "/usr/lib/python3.11/threading.py", line 1038, in _bootstrap_inner
-#     self.run()
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/lgpio.py", line 554, in run
-#     cb.func(chip, gpio, level, tick)
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/gpiozero/pins/lgpio.py", line 248, in _call_when_changed
-#     super()._call_when_changed(ticks / 1000000000, level)
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/gpiozero/pins/local.py", line 111, in _call_when_changed
-#     super()._call_when_changed(
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/gpiozero/pins/pi.py", line 618, in _call_when_changed
-#     method(ticks, state)
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/gpiozero/input_devices.py", line 179, in _pin_changed
-#     self._fire_events(ticks, bool(self._state_to_value(state)))
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/gpiozero/mixins.py", line 385, in _fire_events
-#     self._fire_activated()
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/gpiozero/mixins.py", line 431, in _fire_activated
-#     super()._fire_activated()
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/gpiozero/mixins.py", line 348, in _fire_activated
-#     self.when_activated()
-#   File "/home/david/Projects/zvonkohrator/zvonkohrator-pi-5/test.py", line 36, in play_midi_file
-#     midi_command_handler.note_on(msg.note, msg.velocity)
-#   File "/home/david/Projects/zvonkohrator/zvonkohrator-pi-5/MidiCommandHandler.py", line 117, in note_on
-#     self.midi_player.on_note_on(playable_tone)
-#   File "/home/david/Projects/zvonkohrator/zvonkohrator-pi-5/MidiPlayer.py", line 10, in on_note_on
-#     response = self.serial.readline().strip()
-#                ^^^^^^^^^^^^^^^^^^^^^^
-#   File "/home/david/Projects/zvonkohrator/venv/lib/python3.11/site-packages/serial/serialposix.py", line 595, in read
-#     raise SerialException(
-# serial.serialutil.SerialException: device reports readiness to read but returned no data (device disconnected or multiple access on port?)
