70f78501da5e0a770a49412e2e5034bae865d80f
Ondrej Brejla <ondrej@brejla.cz>
2024-12-06 09:00:31 +0100

    Synchronized writing to serial port to avoid potential corrupted messages when note_off is passed from Timer thread.

--- zvonkohrator-pi-5/MidiPlayer.py
+++ zvonkohrator-pi-5/MidiPlayer.py
@@ -1,16 +1,24 @@
+from queue import Queue
+from threading import Thread
+
 from serial import Serial
 
 
 class MidiPlayer:
     def __init__(self, usb_port: str):
         self.serial = Serial(usb_port, 115200)
+        self.notes_queue = Queue()
+        self.worker_thread = Thread(target=self.__write_worker, daemon=True).start()
+
+    def __write_worker(self):
+        while True:
+            playable_tone = self.notes_queue.get()
+            if playable_tone is None:
+                break
+            self.serial.write(f"{playable_tone}\r".encode())
 
     def on_note_on(self, playable_tone: int):
-        self.serial.write(f"{playable_tone}\r".encode())
-        # response = self.serial.readline().strip()
-        # print(response)
+        self.notes_queue.put(playable_tone)
 
     def on_note_off(self, playable_tone: int):
-        self.serial.write(f"{playable_tone * -1}\r".encode())
-        # response = self.serial.readline().strip()
-        # print(response)
+        self.notes_queue.put(playable_tone * -1)
