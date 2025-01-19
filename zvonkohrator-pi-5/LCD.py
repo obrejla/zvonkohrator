bf1cb5e845ab0fa7df23f56a9be53035100503c5
Ondrej Brejla <ondrej@brejla.cz>
2025-01-19 19:41:20 +0100

    Added thread safe behavior to LCD.

--- zvonkohrator-pi-5/LCD.py
+++ zvonkohrator-pi-5/LCD.py
@@ -1,3 +1,5 @@
+from threading import Lock
+
 from LCD1602.LCD1602 import (
     LCD1602,
     LCD_BLINKOFF,
@@ -13,6 +15,7 @@ class LCD:
         self.cols: int = 16
         self.rows: int = 2
         self.lcd_impl: LCD1602 = None
+        self.lock = Lock()
 
     def __get_lcd_impl(self):
         if self.lcd_impl is None:  # + check whether the instance can be created?
@@ -20,19 +23,24 @@ class LCD:
         return self.lcd_impl
 
     def clear(self):
-        self.__get_lcd_impl().clear()
+        with self.lock:
+            self.__get_lcd_impl().clear()
 
     def set_cursor(self, col: int, row: int):
-        self.__get_lcd_impl().setCursor(col, row)
+        with self.lock:
+            self.__get_lcd_impl().setCursor(col, row)
 
     def printout(self, text: str):
-        self.__get_lcd_impl().printout(text)
+        with self.lock:
+            self.__get_lcd_impl().printout(text)
 
     def display_on(self):
-        self.__get_lcd_impl().command(
-            LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF
-        )
-        self.clear()
+        with self.lock:
+            self.__get_lcd_impl().command(
+                LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF
+            )
+            self.clear()
 
     def display_off(self):
-        self.__get_lcd_impl().command(LCD_DISPLAYCONTROL | LCD_DISPLAYOFF)
+        with self.lock:
+            self.__get_lcd_impl().command(LCD_DISPLAYCONTROL | LCD_DISPLAYOFF)
