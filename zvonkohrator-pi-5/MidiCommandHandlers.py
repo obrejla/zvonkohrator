b9c377d1c9bd9eaf8ed29a3f776f10d1e3de2c32
Ondrej Brejla <ondrej@brejla.cz>
2025-02-06 18:40:08 +0100

    Removed break so potential other handlers are called as well.

--- zvonkohrator-pi-5/MidiCommandHandlers.py
+++ zvonkohrator-pi-5/MidiCommandHandlers.py
@@ -10,7 +10,6 @@ class MidiCommandHandlers:
         for command_handler in self.command_handlers:
             if command_handler.handles(command):
                 command_handler.handle(msg, dt)
-                break
 
     def register(self, command_handler: MidiCommandHandler):
         self.command_handlers.append(command_handler)
